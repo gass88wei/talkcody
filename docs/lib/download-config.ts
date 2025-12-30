b78e0be65a09028ecee081faa9a8e6fa89897fa9
kangkaisen <kangkaisen@apache.org>
2025-12-30 22:41:40 +0800

    Update download link

--- docs/lib/download-config.ts
+++ docs/lib/download-config.ts
@@ -1,6 +1,6 @@
 export const DOWNLOAD_CONFIG = {
-  version: '0.2.1',
-  releaseDate: '2025-12-27',
+  version: '0.2.2',
+  releaseDate: '2025-12-30',
 
   github: {
     repo: 'https://github.com/talkcody/talkcody',
@@ -10,9 +10,9 @@ export const DOWNLOAD_CONFIG = {
   },
 
   downloads: {
-    'darwin-aarch64': 'https://cdn.talkcody.com/releases/v0.2.1/TalkCody_0.2.1_aarch64.dmg',
-    'darwin-x86_64': 'https://cdn.talkcody.com/releases/v0.2.1/TalkCody_0.2.1_x64.dmg',
-    'windows-x86_64': 'https://cdn.talkcody.com/releases/v0.2.1/TalkCody_0.2.1_x64_en-US.msi',
-    'linux-x86_64': 'https://cdn.talkcody.com/releases/v0.2.1/TalkCody_0.2.1_amd64.AppImage',
+    'darwin-aarch64': 'https://cdn.talkcody.com/releases/v0.2.2/TalkCody_0.2.2_aarch64.dmg',
+    'darwin-x86_64': 'https://cdn.talkcody.com/releases/v0.2.2/TalkCody_0.2.2_x64.dmg',
+    'windows-x86_64': 'https://cdn.talkcody.com/releases/v0.2.2/TalkCody_0.2.2_x64_en-US.msi',
+    'linux-x86_64': 'https://cdn.talkcody.com/releases/v0.2.2/TalkCody_0.2.2_amd64.AppImage',
   },
 };
